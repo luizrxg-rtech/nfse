1b4c637575406ea2145fbaf4df5d8511599fcd8b
luizrxg <72672131+luizrxg@users.noreply.github.com>
2025-07-03 16:43:25 -0300

    Updated package-lock.json

--- next.config.js
+++ next.config.js
@@ -4,7 +4,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true 
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.css': {
+          loaders: ['@tailwindcss/vite'],
+        },
+      },
+    },
+  },
 };
 
 module.exports = nextConfig;
